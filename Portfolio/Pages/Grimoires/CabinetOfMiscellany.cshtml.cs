785d777415d74840ff5c380e7d61aef4159cdbc6
Scott Falbo <scottfalboart@gmail.com>
2023-07-30 21:48:50 -0700

    added page skeletons

---
+++ Portfolio/Pages/Grimoires/CabinetOfMiscellany.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages.Grimoires
+{
+    public class CabinetOfMiscellanyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

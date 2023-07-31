1ff9683987512398ba79853621cc806e39a9319e
Scott Falbo <scottfalboart@gmail.com>
2023-07-30 23:57:50 -0700

    added additional pages

---
+++ Portfolio/Pages/Grimoires/Chronicle.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages.Grimoires
+{
+    public class ChronicleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

02731c7971dee542ed07c5676f7d25545060410a
Scott Falbo <scottfalboart@gmail.com>
2023-08-01 00:19:16 -0700

    added constants for contract types

---
+++ Portfolio/MechanistTower/Entities/EternalSymbols/ChronicleCodes.cs
@@ -0,0 +1,19 @@
+﻿namespace Portfolio.MechanistTower.Entities.EternalSymbols
+{
+    public static class ChronicleCodes
+    {
+        public const string FleshRites = nameof(FleshRites);
+
+        public const string Illustration = nameof(Illustration);
+
+        public const string CabinetOfMiscellany = nameof(CabinetOfMiscellany);
+
+        public const string BinaryConjuring = nameof(BinaryConjuring);
+
+        public const string TelekineticTether = nameof(TelekineticTether);
+
+        public const string Chronicle = nameof(Chronicle);
+
+        public const string RandomScroll = nameof(RandomScroll);
+    }
+}
---
+++ Portfolio/MechanistTower/Entities/InfernalContract.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio.MechanistTower.Entities
+{
+    public class InfernalContract
+    {
+        public string id { get; set; } = Guid.NewGuid().ToString();
+
+        public string partitionKey { get; set; }
+    }
+}

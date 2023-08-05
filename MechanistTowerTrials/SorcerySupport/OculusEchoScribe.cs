84c35804e15c2340431277c0454e020d45db03a7
Scott Falbo <scottfalboart@gmail.com>
2023-08-04 23:11:32 -0700

    added mapperly and tests

---
+++ MechanistTowerTrials/SorcerySupport/OculusEchoScribe.cs
@@ -0,0 +1,93 @@
+﻿using Portfolio.MechanistTower.Entities;
+
+namespace MechanistTowerTrials.SorcerySupport
+{
+    internal class OculusEchoScribe
+    {
+        private string _altText = "alt-text";
+        private DateTimeOffset _createdDateTime = DateTimeOffset.Now;
+        private bool _display = true;
+        private string _imageUrl = "test-url";
+        private bool _isForSale;
+        private string _medium;
+        private string _name;
+        private decimal _price;
+        private string _size;
+        private string _thumbnailUrl = "thumbnail-url";
+
+        public OculusEchoScribe WithAltText(string altText)
+        {
+            _altText = altText;
+            return this;
+        }
+
+        public OculusEchoScribe WithCreatedDateTime(DateTimeOffset createdDateTime)
+        {
+            _createdDateTime = createdDateTime;
+            return this;
+        }
+
+        public OculusEchoScribe WithDisplay(bool display)
+        {
+            _display = display;
+            return this;
+        }
+
+        public OculusEchoScribe WithImageUrl(string imageUrl)
+        {
+            _imageUrl = imageUrl;
+            return this;
+        }
+
+        public OculusEchoScribe WithIsForSale(bool isForSale)
+        {
+            _isForSale = isForSale;
+            return this;
+        }
+
+        public OculusEchoScribe WithMedium(string medium)
+        {
+            _medium = medium;
+            return this;
+        }
+
+        public OculusEchoScribe WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public OculusEchoScribe WithPrice(decimal price)
+        {
+            _price = price;
+            return this;
+        }
+
+        public OculusEchoScribe WithSize(string size)
+        {
+            _size = size;
+            return this;
+        }
+
+        public OculusEchoScribe WithThumbnailUrl(string thumbnailUrl)
+        {
+            _thumbnailUrl = thumbnailUrl;
+            return this;
+        }
+
+        public FleshRite BuildFleshRite()
+        {
+            var fleshRite = new FleshRite()
+            {
+                AltText = _altText,
+                CreatedDateTime = _createdDateTime,
+                Display = _display,
+                ImageUrl = _imageUrl,
+                Name = _name,
+                ThumbnailUrl = _thumbnailUrl
+            };
+
+            return fleshRite;
+        }
+    }
+}
---
+++ MechanistTowerTrials/TransmutatorTrials.cs
@@ -0,0 +1,74 @@
+using MechanistTowerTrials.SorcerySupport;
+using Portfolio.MechanistTower.Entities;
+using Portfolio.MechanistTower.Entities.EternalSymbols;
+using Portfolio.MechanistTower.Transmutators;
+
+namespace MechanistTowerTrials
+{
+    [TestClass]
+    public class TransmutatorTrials
+    {
+        [TestMethod]
+        public void FleshRiteTransmutator_FleshRiteToInfernalContract()
+        {
+            var date = DateTimeOffset.Now;
+
+            var fleshRite = new OculusEchoScribe()
+                .WithName("test-name")
+                .WithAltText("test-alt-text")
+                .WithCreatedDateTime(date)
+                .WithDisplay(true)
+                .WithImageUrl("test-image-url")
+                .WithThumbnailUrl("test-thumbnail-url")
+                .BuildFleshRite();
+
+            var transmutator = new FleshRiteTransmutator();
+
+            var infernalContract = transmutator.FleshRiteToInfernalContract(fleshRite);
+
+            Assert.IsNotNull(infernalContract);
+            Assert.AreEqual(fleshRite.Name, infernalContract.Name);
+            Assert.AreEqual(fleshRite.AltText, infernalContract.AltText);
+            Assert.AreEqual(fleshRite.CreatedDateTime, infernalContract.CreatedDateTime);
+            Assert.AreEqual(fleshRite.Display, infernalContract.Display);
+            Assert.AreEqual(fleshRite.ImageUrl, infernalContract.ImageUrl);
+            Assert.AreEqual(fleshRite.ThumbnailUrl, infernalContract.ThumbnailUrl);
+            Assert.AreEqual(fleshRite.EternalSymbol, OculusEchoCyphers.FleshRites);
+        }
+
+        [TestMethod]
+        public void FleshRiteTransmutator_InfernalContractToFleshRite()
+        {
+            var date = DateTimeOffset.Now;
+            var id = Guid.NewGuid().ToString();
+
+            var infernalContract = new InfernalContractScribe()
+                .WithName("test-name")
+                .WithAltText("test-alt-text")
+                .WithCreatedDateTime(date)
+                .WithDisplay(true)
+                .WithEternalSymbol(OculusEchoCyphers.FleshRites)
+                .WithId(id)
+                .WithImageUrl("test-image-url")
+                .WithPartitionKey($"{OculusEchoCyphers.FleshRites}-{id}")
+                .WithThumbnailUrl("test-thumbnail-url")
+                .WithEternalSymbol(OculusEchoCyphers.FleshRites)
+                .Build();
+
+            var transmutator = new FleshRiteTransmutator();
+
+            var fleshRite = transmutator.InfernalContractToFleshRite(infernalContract);
+
+            Assert.IsNotNull(fleshRite);
+            Assert.AreEqual(infernalContract.Name, fleshRite.Name);
+            Assert.AreEqual(infernalContract.AltText, fleshRite.AltText);
+            Assert.AreEqual(infernalContract.CreatedDateTime, fleshRite.CreatedDateTime);
+            Assert.AreEqual(infernalContract.Display, fleshRite.Display);
+            Assert.AreEqual(infernalContract.EternalSymbol, fleshRite.EternalSymbol);
+            Assert.AreEqual(infernalContract.Id, fleshRite.Id);
+            Assert.AreEqual(infernalContract.ImageUrl, fleshRite.ImageUrl);
+            Assert.AreEqual(infernalContract.PartitionKey, fleshRite.PartitionKey);
+            Assert.AreEqual(infernalContract.ThumbnailUrl, fleshRite.ThumbnailUrl);
+        }
+    }
+}

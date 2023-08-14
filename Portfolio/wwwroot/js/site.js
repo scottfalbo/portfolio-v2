5172549a20e9b7e8fa77f61ce5c546b974e33667
Scott Falbo <scottfalboart@gmail.com>
2023-08-13 23:45:35 -0700

    pulled gallery viewer out to full screen full, added open close toggles

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 ﻿'use strict'
+
+$(document).ready(function(){
+  $('.close-gallery').on('click', function(){
+      $('.echo-viewer-data-gallery').addClass('hide-me');
+  });
+});
+
+$(document).ready(function(){
+  $('.open-gallery').on('click', function(){
+      $('.echo-viewer-data-gallery').removeClass('hide-me');
+  });
+});

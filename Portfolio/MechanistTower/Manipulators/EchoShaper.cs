fb2416f9545b963a8442e08051c308bba78043b8
Scott Falbo <scottfalboart@gmail.com>
2023-08-22 22:12:59 -0700

    added loading box

--- Portfolio/MechanistTower/Manipulators/EchoShaper.cs
+++ Portfolio/MechanistTower/Manipulators/EchoShaper.cs
@@ -24,14 +24,11 @@ namespace Portfolio.MechanistTower.Manipulators
             using var echo = Image.Load(file.OpenReadStream());
             var stream = new MemoryStream();
 
-            // Resize with respect to the height
             var newWidth = (int)((double)echo.Width / echo.Height * height);
             echo.Mutate(x => x.Resize(newWidth, height));
 
-            // Check if new width exceeds maxWidth
             if (newWidth > maxWidth)
             {
-                // Determine how much to crop from each side
                 var cropAmount = (newWidth - maxWidth) / 2;
 
                 var cropRectangle = new Rectangle(cropAmount, 0, maxWidth, height);
